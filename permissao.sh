332a2a2002793e2e9d336b711387b4885be37fc5
Kaykybo <122816458+Kaykybo@users.noreply.github.com>
2023-01-31 21:23:34 -0300

    Add files via upload

---
+++ permissao.sh
@@ -0,0 +1,10 @@
+#############################################################################
+#Nome Script: chmod +X 
+#Descrição: Concede permissão a todos os script.
+#Versão: 0.0
+#Licença: GNU
+#Autor: Kayky Batista Oliveira
+#E-mail: kaykybatistaoliveira@outlook.com
+#############################################################################
+#!/bin/bash
+sudo chmod +x *.sh
